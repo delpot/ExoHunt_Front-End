0cd8446b7782d4301c1fd15499a33205d260649d
Delphine Pothin <88280654+delpot@users.noreply.github.com>
2023-09-16 23:21:00 +0200

    style: add webapp introduction

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,14 +12,35 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements AfterViewInit {
-  @ViewChild('videoElement')
-  videoElement!: ElementRef;
+  @ViewChild('videoElement') videoElement!: ElementRef;
 
-  constructor() {}
+  constructor(private el: ElementRef) {}
 
   ngAfterViewInit(): void {
     const video: HTMLVideoElement = this.videoElement.nativeElement;
     video.playbackRate = 0.6;
     video.play();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    const paragraphTriggers = [80, 110, 210];
+
+    for (let i = 1; i <= paragraphTriggers.length; i++) {
+      const paragraph = this.el.nativeElement.querySelector(`.paragraph-${i}`);
+      if (paragraph) {
+        if (scrollPosition >= paragraphTriggers[i - 1]) {
+          paragraph.classList.add('sticky');
+        } else {
+          paragraph.classList.remove('sticky');
+        }
+      }
+    }
   }
 }
